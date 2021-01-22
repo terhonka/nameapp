0efcc24f311d0eceb7a713667c7cef89de44baf0
Tero Honkanen <tero.t.honkanen@gmail.com>
2021-01-22 11:43:35 +0200

    simple UI with buttons select and a text box

--- src/index.js
+++ src/index.js
@@ -1,68 +1,53 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 import axios from 'axios'
 
 
-const ListByAmount = (props) => {
-  const names = props.names
-  
+const ListByAmount = ({ names }) => {
   return (
-  <div>
-    <ul>
-      {names
-      .sort((a, b) => (b.amount > a.amount) ? 1 : -1)
-      .map(n => <li key={n.name}>{n.name} {n.amount} </li>) 
-      }
-    </ul>
-  </div>
+    <table>
+      <tbody>
+        {names
+        .sort((a, b) => (b.amount > a.amount) ? 1 : -1)
+        .map(n => <tr key={n.name}><td>{n.name}</td><td>{n.amount}</td></tr>)
+        }
+      </tbody>
+    </table>
   )
 }
 
-const ListByName = (props) => {
-  const names = props.names
-  
+const ListByName = ({ names }) => {
   return (
-  <div>
-    <ul>
-      {names
-      .sort((a, b) => (a.name > b.name) ? 1 : -1)
-      .map(n => <li key={n.name}>{n.name} </li>)  
-      }
-    </ul>
-  </div>
+    <table>
+      <tbody>
+        {names
+        .sort((a, b) => (a.name > b.name) ? 1 : -1)
+        .map(n => <tr key={n.name}><td>{n.name}</td></tr>)  
+        }
+    </tbody>
+    </table>
   )
 }
 
-const TotalNumberOfNames = (props) => {
-  const names = props.names
-
+const TotalNumberOfNames = ({ names }) => {
   return (
-    <div>
-      <p>
-        Total number of names is {
-        names
-        .map(n => n.amount)
-        .reduce((total, number) => total + number, 0)}   
-      </p>
-    </div>
+    <p>
+      Total number of names is {
+      names
+      .map(n => n.amount)
+      .reduce((total, number) => total + number, 0)}   
+    </p>  
   )
 }
 
-const AmountOfName = (props) => {
-  const names = props.names
-  const aName = props.aName
-
+const AmountOfName = ({ names, aName }) => {
   if ((names.map(n => n.name)).includes(aName)) {
     return (
-      <div>
-        <p> Amount of names {aName} is {names.find(n => n.name === aName).amount} </p>
-    </div>
+      <p> Amount of names {aName} is {names.find(n => n.name === aName).amount} </p>
     )
   } else {
     return (
-      <div>
-        <p> Amount of names {aName} is 0 </p>
-      </div>
+      <p> Amount of names {aName} is 0 </p>
     )
   }
 }
@@ -73,8 +58,19 @@ const Button = (props) => (
   </button>
 )
 
-const ShowListing = (props) => {
-  const {names, listing, aName} = props
+const NameForm = ({nameStr, handleNameChange}) => {
+  return (
+    <form>
+      Specify a name
+      <input
+        value={nameStr}
+        onChange={handleNameChange}
+      />
+    </form>
+  )
+}
+
+const ShowListing = ({ names, listing, nameStr, handleNameChange }) => {
   if (listing === 0) {
     return <ListByAmount names={names} />
   } else if (listing === 1) {
@@ -82,20 +78,38 @@ const ShowListing = (props) => {
   } else if (listing === 2) {
     return <TotalNumberOfNames names={names} />
   } else if (listing === 3) {
-    return <AmountOfName names={names} aName={aName} />
+    return (
+      <div>
+        <NameForm nameStr={nameStr} handleNameChange={handleNameChange} />
+        <AmountOfName names={names} aName={nameStr} />
+      </div>
+    )
   } 
-
 }
 
+const UiButtons = ({ setListing }) => {
+  return (
+    <div>
+      <Button handleClick={() => setListing(0)} text = "Show all names and amounts ordered by amount" />
+      <Button handleClick={() => setListing(1)} text = "Show all names in alphabetical order" />
+      <Button handleClick={() => setListing(2)} text = "Show sum of all names" />
+      <Button handleClick={() => setListing(3)} text = "Show amount of name specified in the text box below" />
+    </div>
+  )
+}
 
-const App = (props) => {
-  const names = props.names
+const App = ({ names }) => {
   const [listing, setListing] = useState(0)
+  const [nameStr, setNameStr] = useState('')
+  
+  const handleNameChange = (event) => {
+    setNameStr(event.target.value)
+  }
   
   return (
     <div>
-      <Button handleClick={() => setListing((listing + 1) % 4)} text = "Next listing" />
-      <ShowListing names={names} listing={listing} aName="Mikko" />
+      <UiButtons setListing={setListing} />
+      <ShowListing names={names} listing={listing} nameStr={nameStr} handleNameChange={handleNameChange} />
     </div>
   )
 }
